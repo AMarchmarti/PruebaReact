37ef51dc605dc80d016ff649085eff805b7cff58
Toni March <amarch@brujula.es>
2019-04-17 11:36:10 +0200

    añadira un boton

--- albums/src/App.js
+++ albums/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
@@ -11,6 +12,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload HOLA .
           </p>
+          <Button>
+            Learn React
+          </Button>
           <a
             className="App-link"
             href="https://reactjs.org"
